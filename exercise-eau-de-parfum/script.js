f5639093f3f35ff0181b9aa8fe943498644999cd
Hazerin <alby91v@gmail.com>
2025-07-29 10:35:24 +0200

    Aggiunto javascript nell'esercizio

---
+++ exercise-eau-de-parfum/script.js
@@ -0,0 +1,15 @@
+function setParfumImage() {
+    if (document.documentElement.clientWidth < 768) {
+        console.log(document.querySelector(".product-image"))
+        document.querySelector(".product-image").src = "image-product-mobile.jpg"
+    } else {
+        document.querySelector(".product-image").src = "image-product-desktop.jpg"
+    }
+}
+
+
+addEventListener("resize", (event) => {
+    setParfumImage()
+})
+
+setParfumImage()
